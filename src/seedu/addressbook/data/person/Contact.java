e41561bf1757c2e48d5652fa209e09384cecbfdb
Ryan Ang <ryanang.jy@gmail.com>
2018-02-11 15:39:47 +0800

    Address, Phone and Email classes made to be a child of Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents a Person's contact in the address book.
+ */
+public class Contact {
+    public String value;
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
