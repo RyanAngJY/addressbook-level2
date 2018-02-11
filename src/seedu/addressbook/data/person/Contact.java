5974e348e0bc7e736e53bfb46144ee91c0ab7bc2
Ryan Ang <ryanang.jy@gmail.com>
2018-02-11 15:45:33 +0800

    Overlapping methods and attributes between Address, Phone and Email classes pushed into Contact class

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -5,9 +5,19 @@ package seedu.addressbook.data.person;
  */
 public class Contact {
     public String value;
+    protected boolean isPrivate;
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
 
     @Override
     public String toString() {
         return this.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
     }
 }
